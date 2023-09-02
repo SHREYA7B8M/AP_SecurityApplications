7615428ff777eb121f02afcea7ceebcf64448c20
s66829020@gmail.com <s66829020@gmail.com>
2023-09-02 23:30:46 +0530

    first commit

---
+++ SecurityApplications/SecurityApplications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityApplications.Startup))]
+namespace SecurityApplications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
